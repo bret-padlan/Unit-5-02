205cfa05456b6663811f72ce705948c616354490
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-10 19:34:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function clickyBot() {
+  document.getElementById("ex").innerHTML = "It works! Hello, world!!!";
+}
+
+function clickyBot2() {
+  document.getElementById("ex").innerHTML = "paragraph, paragraph, paragraph, paragraph, ooooh"
+}
+
+function duckBot() {
+  document.getElementById("img").src = "./images/random_pics.jpg"
+  document.getElementById("img").alt = "duck"
+  document.body.style.backgroundColor = "chocolate"
+}
+
+function boneBot() {
+  document.getElementById("img").src = "./images/boney.jpg"
+  document.getElementById("img").alt = "bone"
+  document.body.style.backgroundColor = "lightblue"
+}
